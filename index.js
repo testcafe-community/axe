5edcc5c87c4255ab0eb65bb63b5a95c2f4cdf9e0
a0k00e3 <akoshti@walmartlabs.com>
2020-06-12 11:35:20 -0700

    fix: update check violation checks, typings

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-const fs = require('fs');
-const path = require('path');
 const { ClientFunction } = require('testcafe');
 const { red, green, reset } = require('chalk');
-const {t} = require('testcafe');
 
 const runAxe = ClientFunction((context, options = {}) => {
     return new Promise((resolve) => {
@@ -18,24 +15,23 @@ const createReport = violations => {
     }
 
     const report = violations.reduce((acc, { nodes, help }, i) => {
+        acc += red(`${i + 1}) ${help}\n`);
 
-        acc += red(`${i+1}) ${help}\n`);
-        
         acc += reset(nodes.reduce((e, { target }) => {
             const targetNodes = target.map((t) => `"${t}"`).join(', ');
             e += `\t${targetNodes}\n`;
             return e;
-        },''));
+        }, ''));
 
         return acc;
 
     }, red(`${violations.length} violations found:\n`));
 
     return reset(report);
-    
+
 };
 
-const axeCheck = async (context, options) => {
+const axeCheck = async (t, context, options) => {
     try {
         return await runAxe.with({ boundTestRun: t })(context, options);
     } catch (e) {
@@ -43,15 +39,14 @@ const axeCheck = async (context, options) => {
     }
 };
 
-const checkForViolations = ({numAllowed=0,context,options}) => {
-    const {violations} = await axeCheck(context, options);
+const checkForViolations = async (t, context, options) => {
+    const { violations = [] } = await axeCheck(t, context, options);
 
-    await t.expect(violations.length <= numAllowed).ok(createReport(violations));
-
-    return {violations};
+    await t.expect(violations.length === 0).ok(createReport(violations));
 }
 
 module.exports = {
     axeCheck,
-    createReport
+    createReport,
+    checkForViolations
 };
